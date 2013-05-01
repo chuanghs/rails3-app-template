e68373dbeb512e17e242bb56cf57b59e2396929e
Zombie <chuanghs@gmail.com>
2013-05-01 11:53:54 +0800

    upgrade to rails3 and using rspec by default

--- Gemfile
+++ Gemfile
@@ -1,75 +1,104 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.7'
+gem 'rails', '3.2.10'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-platforms :ruby do
-  gem 'sqlite3'
-end
-
 platforms :jruby do
-  gem 'activerecord-jdbc-adapter'
-
-  # As rails --database switch does not support derby, hsqldb, h2 nor mssql
-  # as valid values, if you are not using SQLite, comment out the SQLite gem
-  # below and uncomment the gem declaration for the adapter you are using.
-  # If you are using oracle, db2, sybase, informix or prefer to use the plain
-  # JDBC adapter, comment out all the adapter gems below.
-
-  # SQLite JDBC adapter
-  #gem 'jdbc-sqlite3', :require => false
-
-  # Derby JDBC adapter
-  #gem 'activerecord-jdbcderby-adapter'
-
-  # HSQL JDBC adapter
-  #gem 'activerecord-jdbchsqldb-adapter'
-
-  # H2 JDBC adapter
-  gem 'activerecord-jdbch2-adapter'
-
-  # SQL Server JDBC adapter
-  #gem 'activerecord-jdbcmssql-adapter'
-
-  # PostgreSQL JDBC adapter 
+  gem 'activerecord-jdbcsqlite3-adapter'
   gem 'activerecord-jdbcpostgresql-adapter'
+  gem 'activerecord-oracle_enhanced-adapter'
+
+  gem 'jruby-openssl'
+
+  # poi wrapper
+  gem 'jruby-poi', :git => 'git://github.com/chuanghs/jruby-poi.git' # use my personal modification
+
+  # deploy on javaee app server
+  gem 'warbler'
 end
 
-gem 'compass', '>= 0.11.1'
 
-#gem 'yajl-ruby'
+# Gems used only for assets and not required in production environments by default
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'compass-rails'
+  gem 'coffe-rails', '~> 3.2.1'
 
-# Use unicorn as the web server
-# gem 'unicorn'
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  gem 'therubyrhino'
 
-# Deploy with Capistrano
-# gem 'capistrano'
+  gem 'uglifier', '>= 1.0.3'
+end
 
-# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
-# gem 'ruby-debug'
-# gem 'ruby-debug19', :require => 'ruby-debug'
+gem 'jquery-rails'
 
-# Bundle the extra gems:
-# gem 'bj'
-# gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
+gem 'nested_scaffold'
 
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
-group :development, :test do
+# security (handling authority)
+gem 'authority'
+
+group :test, :development do
+end
+
+
+group :development do
+  gem 'guard'
+  gem 'guard-rails'
+  gem 'guard-sass'
+  gem 'guard-livereload'
+
+  # using test
+#  gem 'guard-test'
+  # using rspec
+  gem 'guard-rspec'
+
+  # guard notification 
+  gem 'terminal-notifier-guard'
+  gem 'rb-inotify', :require => false
+  gem 'rb-fsevent', :require => false
+  gem 'rb-fchange', :require => false
+
+end
+
+group :test do
+  gem 'simplecov'
+
+  #using test
+ # gem 'mocha', '=0.12.1', :require => false  # fix on 0.12.1, 0.13 not work
+
+  #using rspec
+  gem 'rspec-rails', '~> 2.0'
   gem 'factory_girl_rails'
   gem 'shoulda-matchers'
-  gem 'rcov'
-  gem 'delorean'
-  gem 'watchr'
-#   gem 'webrat'
+  
 end
 
 
+# xml handling
+gem 'nokogiri'
+
+# Paginator
+# gem "kaminari"
+# gem 'will_paginate', '3.0.pre2'
+
+# File Uploads
+# gem 'paperclip'
+# gem 'carrierwave'
+
+# gem 'devise', '~> 1.1.7'
+# gem "cancan"
+# gem "simple_form"
+# gem 'fastercsv' # for Ruby 1.8.7+
+# gem 'prawn'
+# gem "haml"
+# gem "haml-rails"
+# gem 'validates_timeliness', '~> 3.0.5'
+# gem 'acts-as-taggable-on'
+ 
+# gem 'hoptoad_notifier'
+# gem 'newrelic_rpm'
 
 # State machine
 # gem 'aasm'
@@ -79,7 +108,7 @@ end
 # gem 'typhoeus'
 # gem 'rest-client', :require => 'restclient'
 
-# Memcached client
+# Memcached client 
 # gem "dalli"
 # gem "memcache-client", :require => "memcache"
 
@@ -97,4 +126,3 @@ end
 
 # Full-text search engine
 # gem 'thinking-sphinx', '~> 2.0.2', :require => 'thinking_sphinx'
-
--- rails3.rb
+++ rails3.rb
@@ -2,114 +2,59 @@
 run "rm README"
 run "rm public/index.html"
 run "rm public/images/rails.png"
-run "rm app/views/layouts/application.html.erb"
-
-# remove Prototype defaults
-run "rm public/javascripts/controls.js"
-run "rm public/javascripts/dragdrop.js"
-run "rm public/javascripts/effects.js"
-run "rm public/javascripts/prototype.js"
-
-# add XHTML 1.0 Strict layout, with jQuery from Google
-file 'app/views/layouts/application.html.erb', <<-ERB
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Enterprise</title>
-  <%= stylesheet_link_tag 'screen.css', :media => 'screen, projection' %>
-  <%= stylesheet_link_tag 'print.css', :media => 'print' %>
-  /[if lt IE 8]
-  <%= stylesheet_link_tag 'ie.css', :media => 'screen, projection' %>
-
-  <%= javascript_include_tag :defaults %>
-  <%= csrf_meta_tag %>
-</head>
-<body>
-
-<%= yield %>
-
-</body>
-</html>
-ERB
-
-
-
-# avoid mysql2 option for Rails3
-if options[:database] =~/mysql/
-  gsub_file "config/database.yml", /mysql2/, 'mysql'
-end
-
-#Add comments to config/database
-file = "config/database.yml"
-
-comment = <<COMMENTS
-# If you are using derby, hsqldb, h2 or mssql with one of the ActiveRecord
-# JDBC adapters, configure your database setting as the example below:
-#
-#development:
-#  adapter: mssql
-#  username: <username>
-#  password: <password>
-#  hostname: localhost
-#  database: dummy
-
-# If you are using oracle, db2, sybase, informix or prefer to use the plain
-# JDBC adapter, configure your database setting as the example below (requires
-# you to download and manually install the database vendor's JDBC driver .jar
-# file). See your driver documentation for the apropriate driver class and
-# connection string:
-#
-#development:
-#  adapter: jdbc
-#  username: <username>
-#  password: <password>
-#  driver: com.ibm.db2.jcc.DB2Driver
-#  url: jdbc:db2://localhost:5021/dummy
-
-COMMENTS
-temp = IO.read(file)
-open(file, "w") {|f| f << comment << temp }
 run "cp config/database.yml config/database.yml.example"
 
 # install gems
 run "rm Gemfile"
 file 'Gemfile', File.read("#{File.dirname(rails_template)}/Gemfile")
 
-#bundle install
+# bundle install
 run "bundle install"
 
-# copy files"
-file "script/watchr.rb", File.read("#{File.dirname(rails_template)}/watchr.rb")
+# generate rspec
+generate "rspec:install"
+
+# generate Guard file
+run "guard init livereload rspec sass"
+
+# copy files
+file 'script/watchr.rb', File.read("#{File.dirname(rails_template)}/watchr.rb")
 file 'lib/tasks/dev.rake', File.read("#{File.dirname(rails_template)}/dev.rake")
+
+# remove active_resource and test_unit
+gsub_file 'config/application.rb', /require 'rails\/all'/, <<-CODE
+  require 'rails'
+  require 'active_record/railtie'
+  require 'action_controller/railtie'
+  require 'action_mailer/railtie'
+CODE
 
 # install jquery
 run "curl -L http://code.jquery.com/jquery.min.js > public/javascripts/jquery.js"
 run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
 
-gsub_file 'config/application.rb', /(config.action_view.javascript_extensions.*)/,
-                                   "config.action_view.javascript_extensions[:defaults] = %w(jquery rails)"
+gsub_file 'config/application.rb', /(config.action_view.javascript_expansions.*)/, 
+                                   "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)"
 
 # add time format
-#environment 'Time::DEFAULT_FORMAT.merge!(:default => "%Y/%m/%d %I:%M %p", :ymd => "%Y/%m/%d")'
 environment 'Time::DATE_FORMATS.merge!(:default => "%Y/%m/%d %I:%M %p", :ymd => "%Y/%m/%d")'
 
-run "compass init rails . --using blueprint"
-
-#.gitignore
+# .gitignore
 append_file '.gitignore', <<-CODE
 config/database.yml
 Thumbs.db
 .DS_Store
 tmp/*
+coverage/*
 *~
+.project
 CODE
 
-
-# keep time and log
+# keep tmp and log
 run "touch tmp/.gitkeep"
 run "touch log/.gitkeep"
 
-#git commit
+# git commit
 git :init
 git :add => '.'
 git :add => 'tmp/.gitkeep -f'
